cff99bcd3ba05a5e40438dd175f9d75b46db1545
BlueKitsune狐 <saadshaan619@gmail.com>
2025-09-19 18:04:31 +0500

    first commit

---
+++ src/data/personal.js
@@ -0,0 +1,24 @@
+let userProfile = {
+  avatar_url: "https://avatars.githubusercontent.com/u/116144344?v=4",
+  name: "Saad Yousuf",
+  location: "Pakistan, Karachi",
+  email: "saadyousu64@gmail.com",
+  twitter_Link: "https://x.com/sad_yosuf",
+  github_Link: "https://github.com/bluekitsune-sad",
+  linkedin_Link: "https://www.linkedin.com/in/syed-saad-yousuf-raza-a83389243/",
+  tech_stack: [
+    "React",
+    "Next.js",
+    "Tailwind CSS",
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "Docker",
+    "AWS",
+    "Azure",
+    "Bash",
+    "Python",
+  ],
+};
+
+export default userProfile;
